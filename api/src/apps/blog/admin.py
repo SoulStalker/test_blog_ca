d39303f6a478f9e877ac1bf2346d3e7631c24853
Almaz Galimov <almazwork@gmail.com>
2024-09-14 23:00:22 +0500

    add admin panel

--- api/src/apps/blog/admin.py
+++ api/src/apps/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from src.models.blog import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'status', 'publish')
+    list_filter = ('status', 'author', 'publish')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish'
+    raw_id_fields = ('author',)
+    ordering = ['status', '-publish']
+
+
--- api/src/models/blog.py
+++ api/src/models/blog.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -9,7 +10,6 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
@@ -35,3 +35,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail",
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
