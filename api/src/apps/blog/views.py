a64a384d1d2eb191117fd3ac3b75317b50eb6933
Almaz Galimov <almazwork@gmail.com>
2024-09-15 22:50:12 +0500

    add pagination

--- api/src/apps/blog/views.py
+++ api/src/apps/blog/views.py
@@ -1,5 +1,17 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from src.models.blog import Post
+
+from api.src.apps.blog.forms import EmailPostForm
+from api.src.config import settings
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -18,3 +30,24 @@ def post_detail(request, year, month, day, post):
     )
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"Somebody recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\ncomments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post, 'form': form, 'sent': sent})
